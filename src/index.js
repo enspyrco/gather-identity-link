2a21e3cf6ff9c1ab8ea67f747c3af03ca792df58
Nick Meinhold <nick@enspyr.co>
2022-07-01 23:45:27 +1000

    first commit

---
+++ src/index.js
@@ -0,0 +1,93 @@
+import './styles.css';
+import { 
+  hideLoginError, 
+  showLoginState, 
+  showLoginForm, 
+  showApp, 
+  showLoginError, 
+  btnLogin,
+  btnSignup,
+  btnLogout
+} from './ui'
+
+import { initializeApp } from 'firebase/app';
+import { 
+  getAuth,
+  onAuthStateChanged, 
+  signOut,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  connectAuthEmulator
+} from 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBUdxGEMisEGRdDrIl0L7tD1AfiiSi0PC0",
+  authDomain: "gather-identity-link.firebaseapp.com",
+  projectId: "gather-identity-link",
+  storageBucket: "gather-identity-link.appspot.com",
+  messagingSenderId: "865555838574",
+  appId: "1:865555838574:web:06f5aba18dfda488ee187f"
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+// Login using email/password
+const loginEmailPassword = async () => {
+  const loginEmail = txtEmail.value
+  const loginPassword = txtPassword.value
+
+  try {
+    await signInWithEmailAndPassword(auth, loginEmail, loginPassword)
+  }
+  catch(error) {
+    console.log(`There was an error: ${error}`)
+    showLoginError(error)
+  }
+}
+
+// Create new account using email/password
+const createAccount = async () => {
+  const email = txtEmail.value
+  const password = txtPassword.value
+
+  try {
+    await createUserWithEmailAndPassword(auth, email, password)
+  }
+  catch(error) {
+    console.log(`There was an error: ${error}`)
+    showLoginError(error)
+  } 
+}
+
+// Monitor auth state
+const monitorAuthState = async () => {
+  onAuthStateChanged(auth, user => {
+    if (user) {
+      console.log(user)
+      showApp()
+      showLoginState(user)
+
+      hideLoginError()
+      hideLinkError()
+    }
+    else {
+      showLoginForm()
+      lblAuthState.innerHTML = `You're not logged in.`
+    }
+  })
+}
+
+// Log out
+const logout = async () => {
+  await signOut(auth);
+}
+
+btnLogin.addEventListener("click", loginEmailPassword) 
+btnSignup.addEventListener("click", createAccount)
+btnLogout.addEventListener("click", logout)
+
+const auth = getAuth(firebaseApp);
+connectAuthEmulator(auth, "http://localhost:9099");
+
+monitorAuthState();
