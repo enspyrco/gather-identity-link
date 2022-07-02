537b2e0ead851efbb0ac090c67d8e63609d4b81a
Nick Meinhold <nick@enspyr.co>
2022-07-02 18:00:05 +1000

    Refactor: call-from-anywhere getFirebase function
    Using the pattern as it is suggested as a battel tested method in
    https://www.youtube.com/watch?v=knk5Fjrpde0

---
+++ src/config.js
@@ -0,0 +1,10 @@
+export const config = {
+  firebase: {
+    apiKey: "AIzaSyBUdxGEMisEGRdDrIl0L7tD1AfiiSi0PC0",
+    authDomain: "gather-identity-link.firebaseapp.com",
+    projectId: "gather-identity-link",
+    storageBucket: "gather-identity-link.appspot.com",
+    messagingSenderId: "865555838574",
+    appId: "1:865555838574:web:06f5aba18dfda488ee187f"
+  }
+};
